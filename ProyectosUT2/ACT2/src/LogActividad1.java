f0acae901fc7192741b163a98ff01b7868f2729d
Juca28 <4166363@alu.murciaeduca.es>
2025-09-22 17:41:05 +0200

    Act2 incompleta

---
+++ ProyectosUT2/ACT2/src/LogActividad1.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+
+public class LogActividad1 {
+    public static void main (String[] args) {
+
+        //Creacion del directorio logs/seguridad
+        String ruta = "logs/seguridad";
+        File directorio = new File(ruta);
+
+        if (!directorio.exists()) {
+            boolean existe = directorio.mkdirs();
+            if (existe) {
+                System.out.println("Directorio creado: " + directorio.getAbsolutePath());
+            } else {
+                System.out.println("No se ha podido crear el directorio");
+                return;
+            }
+        } else {
+            System.out.println("El directorio ya existe: " + directorio.getAbsolutePath());
+        }
+
+        //Creacion del fichero de log
+        File fichLog = new File(ruta + "seguridad_actividad1.log");
+        try {
+            if (fichLog.exists()) {
+                System.out.println("El fichero ya existe: " + fichLog.getAbsolutePath());
+            } else {
+                if (fichLog.createNewFile()) {
+                    System.out.println("El fichero se creo correctamente:  " + fichLog.getAbsolutePath());
+                } else {
+                    System.out.println("No se ha podido crear el fichero");
+                }
+            }
+        }catch (IOException e){
+            System.out.println("Error al crear el fichero" + e.getMessage());
+            return;
+        }
+    }
+}
